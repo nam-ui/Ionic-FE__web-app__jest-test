6bcd58757e18690ef3742108490de4c17794e0ad
DESKTOP-5P4MR65\devadmin <namtt@hqsoft.com.vn>
2022-07-05 17:24:20 +0700

    all _ config _ package json

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders without crashing', () => {
-  const { baseElement } = render(<App />);
-  expect(baseElement).toBeDefined();
+test('adds 1 + 2 to equal 3', () => {
+
 });
